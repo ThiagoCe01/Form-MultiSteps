d1802b7ebd6ac34814ea77e6fa9d99866408b6c6
Thiago Clementino <thidf57@gmail.com>
2024-04-09 07:52:08 -0300

    first commit

---
+++ form.js
@@ -0,0 +1,7 @@
+const prevOne =document.getElementById('#previOne');
+const blockform = document.getElementsByClassName('.blockform');
+
+
+previOne.addEventListener('click', () => {
+    blockform.style.marginLeft = '-100%';
+});
